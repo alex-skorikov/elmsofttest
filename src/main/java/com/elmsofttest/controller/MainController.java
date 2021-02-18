2ddc7242e1d60b47e6d97553d89788bb4d7d3102
AlexSkorikov <the.na4ig@gmail.com>
2021-02-19 06:43:35 +1000

    Initial commit

---
+++ src/main/java/com/elmsofttest/controller/MainController.java
@@ -0,0 +1,23 @@
+package com.elmsofttest.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+/**
+ * Main controller.
+ */
+@Controller
+public class MainController {
+    /**
+     * First get.
+     * @return index page.
+     */
+    @RequestMapping(value = "/", method = RequestMethod.GET)
+    public ModelAndView main() {
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.setViewName("index");
+        return modelAndView;
+    }
+}
---
+++ src/main/webapp/WEB-INF/views/index.jsp
@@ -0,0 +1,11 @@
+<%@ taglib prefix="spring" uri="http://www.springframework.org/tags/form" %>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+
+<html>
+<head>
+    <title>Index Page</title>
+</head>
+<body>
+    'Initial commit...'
+</body>
+</html>
